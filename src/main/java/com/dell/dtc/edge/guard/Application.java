ab3cca793de6353f99983ea540a5d37b5a9be4ed
Ifancy Ariaratnam <ifancy_ariaratnam@dell.com>
2023-10-09 18:58:31 -0400

    Updated Controller

--- src/main/java/com/dell/dtc/edge/guard/Application.java
+++ src/main/java/com/dell/dtc/edge/guard/Application.java
@@ -5,9 +5,18 @@ package com.dell.dtc.edge.guard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping("/alert")
+    public String getAlert() {
+        return "get alert is working";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/dell/dtc/edge/guard/controller/IncidentManagementController.java
+++ src/main/java/com/dell/dtc/edge/guard/controller/IncidentManagementController.java
@@ -5,24 +5,16 @@ package com.dell.dtc.edge.guard.controller;
 
 import com.dell.dtc.edge.guard.model.AlertModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 public class IncidentManagementController {
 
-    @PostMapping("/alert")
+    @PostMapping("/api/v1/alert")
     public ResponseEntity<AlertModel> createAlert() {
         AlertModel model = AlertModel.builder().message("alert is working").build();
         return ResponseEntity.ok(model);
-    }
-
-    @GetMapping("/alert")
-    public ResponseEntity<String> getAlert() {
-        return ResponseEntity.ok("get alert is working");
     }
 
 }
