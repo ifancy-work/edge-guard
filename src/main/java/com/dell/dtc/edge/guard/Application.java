401fb0d121db8cfdf76730e072930f699f64c797
Ifancy Ariaratnam <ifancy_ariaratnam@dell.com>
2023-10-10 06:56:19 -0400

    Refactored

--- src/main/java/com/dell/dtc/edge/guard/Application.java
+++ src/main/java/com/dell/dtc/edge/guard/Application.java
@@ -5,7 +5,6 @@ package com.dell.dtc.edge.guard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
